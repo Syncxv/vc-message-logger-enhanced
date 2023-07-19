7a8b9ef6a2f4687383864138d98e3ad52b2d616f
Syncxv <aruldevarackal1@gmail.com>
2023-07-19 19:22:38 +1000

    ok yeah my version was terrible

--- utils/index.ts
+++ utils/index.ts
@@ -66,55 +66,36 @@ export function findLastIndex<T>(array: T[], predicate: (e: T, t: number, n: T[]
 
 // stolen from mlv2
 // https://github.com/1Lighty/BetterDiscordPlugins/blob/master/Plugins/MessageLoggerV2/MessageLoggerV2.plugin.js#L2367
-
-
 interface Id { id: string, time: number; }
-
-const DISCORD_EPOCH = 1420070400000;
-
-function convertIdToTime(id: string): number {
-    return parseInt(id) / 4194304 + DISCORD_EPOCH;
-}
-
-function filterExistingMessages(ids: Id[], messages: LoggedMessage[]): Id[] {
-    return ids.filter(({ id }) => messages.findIndex(msg => msg.id === id) === -1);
-}
-
-function sortIdsByTime(ids: Id[]): Id[] {
-    return ids.slice().sort((a, b) => b.time - a.time);
-}
-
 export function reAddDeletedMessages(messages: LoggedMessage[], deletedMessages: string[], channelStart: boolean, channelEnd: boolean) {
     if (!messages.length || !deletedMessages?.length) return;
+    const DISCORD_EPOCH = 14200704e5;
+    const IDs: Id[] = [];
+    const savedIDs: Id[] = [];
 
-    const IDs: Id[] = messages.map(({ id }) => ({ id, time: convertIdToTime(id) }));
-    const savedIDs: Id[] = deletedMessages
-        .map(id => ({ id, time: convertIdToTime(id) }))
-        .filter(({ id }) => !!loggedMessagesCache[id]);
-
+    for (let i = 0, len = messages.length; i < len; i++) {
+        const { id } = messages[i];
+        IDs.push({ id: id, time: (parseInt(id) / 4194304) + DISCORD_EPOCH });
+    }
+    for (let i = 0, len = deletedMessages.length; i < len; i++) {
+        const id = deletedMessages[i];
+        const record = loggedMessagesCache[id];
+        if (!record) continue;
+        savedIDs.push({ id: id, time: (parseInt(id) / 4194304) + DISCORD_EPOCH });
+    }
     savedIDs.sort((a, b) => a.time - b.time);
-
     if (!savedIDs.length) return;
-
-    const lowestTime = IDs[IDs.length - 1].time;
-    const highestTime = IDs[0].time;
-
+    const { time: lowestTime } = IDs[IDs.length - 1];
+    const [{ time: highestTime }] = IDs;
     const lowestIDX = channelEnd ? 0 : savedIDs.findIndex(e => e.time > lowestTime);
     if (lowestIDX === -1) return;
-
-    const highestIDX = channelStart
-        ? savedIDs.length - 1
-        : savedIDs.slice().reverse().findIndex(e => e.time < highestTime);
+    const highestIDX = channelStart ? savedIDs.length - 1 : findLastIndex(savedIDs, e => e.time < highestTime);
     if (highestIDX === -1) return;
-
-    const reAddIDs = savedIDs.slice(lowestIDX, savedIDs.length - highestIDX);
-
-    const idsToAdd = filterExistingMessages(reAddIDs, messages);
-    idsToAdd.push(...IDs);
-    const sortedIds = sortIdsByTime(idsToAdd);
-
-    for (let i = 0, len = sortedIds.length; i < len; i++) {
-        const { id } = sortedIds[i];
+    const reAddIDs = savedIDs.slice(lowestIDX, highestIDX + 1);
+    reAddIDs.push(...IDs);
+    reAddIDs.sort((a, b) => b.time - a.time);
+    for (let i = 0, len = reAddIDs.length; i < len; i++) {
+        const { id } = reAddIDs[i];
         if (messages.findIndex(e => e.id === id) !== -1) continue;
         const record = loggedMessagesCache[id];
         messages.splice(i, 0, record.message!);
