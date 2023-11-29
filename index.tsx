4110101c418c840251ba857739f18cc2d94224e0
Syncxv <aruldevarackal1@gmail.com>
2023-11-29 21:36:01 +0530

    welp ima finish updater later

--- index.tsx
+++ index.tsx
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-export const VERSION = "1.4.0";
+export const VERSION = "2.0.0";
 
 export const Native = getNative();
 
