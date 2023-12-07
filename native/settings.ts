43fa6469bea8e17e8d1b87da6807be74cb39560e
Syncxv <aruldevarackal1@gmail.com>
2023-12-07 10:29:15 +0530

    remove debug isNew

--- native/settings.ts
+++ native/settings.ts
@@ -24,7 +24,6 @@ export async function getSettings(): Promise<MLSettings> {
         const settings = {
             logsDir: await getDefaultNativeDataDir(),
             imageCacheDir: await getDefaultNativeImageDir(),
-            isNew: true
         };
         try {
             await saveSettings(settings);
